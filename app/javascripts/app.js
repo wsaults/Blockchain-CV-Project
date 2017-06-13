f3aa7fbfd60c6ba8f2a7be020b94858fa609c21a
Will Saults <wsaults@yahoo.com>
2017-06-12 23:47:53 -0500

    Added particles.js - all the cool decentralized kids are doing it these days...

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -43,6 +43,11 @@ window.App = {
 
       self.refreshBalance();
     });
+
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+      particlesJS.load('particles-js', '../app/assets/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+      });
   },
 
   setStatus: function(message) {
