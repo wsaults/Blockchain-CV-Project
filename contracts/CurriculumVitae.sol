d539d3f101cb48ccb07f4c5b14ac06c5f2c5a5e1
Will Saults <wsaults@yahoo.com>
2017-06-13 10:39:13 -0500

    Added test for identity values

--- contracts/CurriculumVitae.sol
+++ contracts/CurriculumVitae.sol
@@ -4,29 +4,37 @@ import "./CVExtender.sol";
 
 contract CurriculumVitae is CVExtender {
     
+    struct Identity {
+        string _name;
+        string _title;
+        string _description;
+        string _url;
+        string _email;
+    }
+
+    Identity identity;
+
+    function setIdentity(string name, string title, string description, string url, string email) {
+        identity = Identity(name, title, description, url, email);
+    }
+     
     /**
-     * Your functions go here
-     * 
-     * */
-     
-     function MyFunction1() {}
-     function MyFunction2() {}
-     
-     
-     /**
-      * Below is for our CV!
-      * */
+     * Below is for our CV!
+     **/
     function getAddress() constant returns(string) {
-        return "http://www.example.org";
+        return identity._url; //"http://www.example.org";
     }
     
     function getDescription() constant returns(string) {
-        return "This is an example";
+        return identity._description; //"This is an example";
     }
+
     function getTitle() constant returns(string) {
-        return "SimpleExample";
+        return identity._title; //"SimpleExample";
     }
+
     function getAuthor() constant returns(string, string) {
-        return ("Thomas", "thomas@example.org");
+        return (identity._name, identity._email); 
+        // ("Thomas", "thomas@example.org");
     }
 }
--- test/curriculumvitae.js
+++ test/curriculumvitae.js
@@ -1,15 +1,41 @@
 var CurriculumVitae = artifacts.require("./CurriculumVitae.sol");
 
 contract('CurriculumVitae', function(accounts) {
-    it('should be a placeholder test', function() {
-        // var contractInstance;
-        // return wallet.deployed().then(function(instance) {
-        //     contractInstance = instance;
-        //     return contractInstance.sendTransaction({value: web3.toWei(10, 'ether'), address: contractInstance.address, from: accounts[0]});
-        // }).then(function(results) {
-        //     assert.equal(web3.eth.getBalance(contractInstance.address).toNumber(), web3.toWei(10, 'ether'), 'The Balance is the same');
-        // });
+    // it('should be a placeholder test', function() {
+    //     // var contractInstance;
+    //     // return wallet.deployed().then(function(instance) {
+    //     //     contractInstance = instance;
+    //     //     return contractInstance.sendTransaction({value: web3.toWei(10, 'ether'), address: contractInstance.address, from: accounts[0]});
+    //     // }).then(function(results) {
+    //     //     assert.equal(web3.eth.getBalance(contractInstance.address).toNumber(), web3.toWei(10, 'ether'), 'The Balance is the same');
+    //     // });
 
-        assert.equal('test', 'test'), 'Sample test';
+    //     assert.equal('test', 'test'), 'Sample test';
+    // });
+
+    it("setIdentity function should fill in values", function() {
+        var contractInstance;
+        return CurriculumVitae.deployed().then(function(instance) {
+            contractInstance = instance;
+            return contractInstance.setIdentity("Will Saults", "Developer", "Some guy learning Solidity", "http://example.com", "email@example.com");
+        }).then(function() {
+            return contractInstance.getAuthor();
+        }).then(function(results) {
+            var [author, email] = results;
+            assert.equal(author, "Will Saults", "The authors are the same");
+            assert.equal(email, "email@example.com", "The emails are the same");
+        }).then(function() {
+            return contractInstance.getTitle();
+        }).then(function(results) {
+            assert.equal(results, "Developer", "The titles are the same");
+        }).then(function() {
+            return contractInstance.getDescription();
+        }).then(function(results) {
+            assert.equal(results, "Some guy learning Solidity", "The descriptions are the same");
+        }).then(function() {
+            return contractInstance.getAddress();
+        }).then(function(results) {
+            assert.equal(results, "http://example.com", "The urls are the same");
+        });
     });
 });
