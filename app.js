2f342c832df064842c15058373e790129c94c408
Will Saults <wsaults@yahoo.com>
2017-06-15 11:34:29 -0500

    asset updates

--- app.js
+++ app.js
@@ -16516,7 +16516,7 @@ window.App = {
     });
 
     /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-    particlesJS.load('particles-js', '../app/assets/particles.json', function() {
+    particlesJS.load('particles-js', './assets/particles.json', function() {
       console.log('callback - particles.js config loaded');
     });
 
