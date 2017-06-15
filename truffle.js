7b4aacad9de20d5ff19f331cd336171d0c225acd
Will Saults <wsaults@yahoo.com>
2017-06-15 13:39:40 -0500

    Added truffle networks

--- truffle.js
+++ truffle.js
@@ -3,6 +3,16 @@ require('babel-register')
 
 module.exports = {
   networks: {
+    live: {
+        network_id: 1, // Ethereum public network
+    },
+    morden: {
+        network_id: 2, // Official Ethereum test network
+        host: '127.0.0.1',
+        port: '8545',
+        network_id: '*',
+        gas: 3000000
+    },
     development: {
       host: 'localhost',
       port: 8545,
