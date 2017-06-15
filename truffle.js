23a43574902e2069e10b5804d893a9a44ac11d3f
Will Saults <wsaults@yahoo.com>
2017-06-15 14:12:22 -0500

    truffle.js fix

--- truffle.js
+++ truffle.js
@@ -5,12 +5,15 @@ module.exports = {
   networks: {
     live: {
         network_id: 1, // Ethereum public network
+        host: '127.0.0.1',
+        port: '8545',
+        gas: 3000000,
+        from: "0x05bC223f5fBd2dca548a070C19d6F183671C76a1",
     },
     morden: {
         network_id: 2, // Official Ethereum test network
         host: '127.0.0.1',
         port: '8545',
-        network_id: '*',
         gas: 3000000
     },
     development: {
