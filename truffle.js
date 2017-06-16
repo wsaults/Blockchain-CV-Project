4e8ca0e43c39552cb1a0a2bfbc3fc0f28fae02a1
Will Saults <wsaults@yahoo.com>
2017-06-16 12:26:30 -0500

    mnemonic updates

--- truffle.js
+++ truffle.js
@@ -1,14 +1,17 @@
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register')
+var dotenv = require('dotenv');
+dotenv.load();
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
   networks: {
     live: {
-        network_id: 1, // Ethereum public network
-        host: '127.0.0.1',
-        port: '8545',
-        gas: 3000000,
-        from: "0x05bC223f5fBd2dca548a070C19d6F183671C76a1"
+      network_id: 1, // Ethereum public network
+      provider: new HDWalletProvider(process.env.MAIN, "https://mainnet.infura.io/"),
+      gas: 4000000,
+      gasPrice: 25000000000
     },
     morden: {
         network_id: 2, // Official Ethereum test network
@@ -16,6 +19,14 @@ module.exports = {
         port: '8545',
         gas: 3000000,
         from: "0x92440b258C944AB5136C82963E1452793CD90eb0"
+    },
+    ropsten: {
+      provider: new HDWalletProvider(process.env.ROPSTEN, "https://ropsten.infura.io/"),
+      network_id: 3
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(process.env.RINKEBY, "https://www.rinkeby.io/"),
+      network_id: 4
     },
     development: {
       host: 'localhost',
